89f0fdf12771c7652a65f821b6e39bfa2356f405
samhuang95 <sam.huang.veda@gmail.com>
2024-06-27 00:16:47 +0800

    Modify - the insert mission function and load SQL lite db data

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,39 +2,86 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// 空白頁項目範本已記錄在 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x404
+using SQLite;
+using Windows.Storage;
+using System.Threading.Tasks;
 
 namespace no_time
 {
-    /// <summary>
-    /// 可以在本身使用或巡覽至框架內的空白頁面。
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string dbPath;
+
         public MainPage()
         {
             this.InitializeComponent();
+            InitializeDatabase();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void InitializeDatabase()
         {
-            MediaElement mediaElement = new MediaElement();
-            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
-            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = 
-                await synth.SynthesizeTextToStreamAsync("Hello");
-            mediaElement.SetSource(stream, stream.ContentType);
-            mediaElement.Play();
+            dbPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "missions.db");
+            using (var db = new SQLiteConnection(dbPath))
+            {
+                db.CreateTable<Mission>();
+            }
         }
+
+        private async void saveButtonClick(object sender, RoutedEventArgs e)
+        {
+            string missionTitle = this.missionTitle.Text;
+            DateTimeOffset? missionDeadline = this.CalendarDatePicker.Date;
+
+            var mission = new Mission
+            {
+                MissionId = Guid.NewGuid().ToString(),
+                UserId = "user_123", // Replace with actual user ID
+                MissionTitle = missionTitle,
+                MissionDeadline = missionDeadline?.DateTime ?? DateTime.Now,
+                IsDeleted = false,
+                CreateId = "user_123", // Replace with actual user ID
+                CreateTime = DateTime.Now,
+                UpdateId = "user_123", // Replace with actual user ID
+                UpdateTime = DateTime.Now
+            };
+
+            await Task.Run(() =>
+            {
+                using (var db = new SQLiteConnection(dbPath))
+                {
+                    db.Insert(mission);
+                }
+            });
+        }
+
+        private async void loadButtonClick(object sender, RoutedEventArgs e)
+        {
+            List<Mission> missions = await Task.Run(() =>
+            {
+                using (var db = new SQLiteConnection(dbPath))
+                {
+                    return db.Table<Mission>().ToList();
+                }
+            });
+
+            MissionListView.ItemsSource = missions;
+        }
+    }
+
+    public class Mission
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string MissionId { get; set; }
+        public string UserId { get; set; }
+        public string MissionTitle { get; set; }
+        public DateTime MissionDeadline { get; set; }
+        public bool IsDeleted { get; set; }
+        public string CreateId { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string UpdateId { get; set; }
+        public DateTime UpdateTime { get; set; }
     }
 }
